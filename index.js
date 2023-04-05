28b8d645654bd5a3825791f74eb8318cd848a7d3
Mutiyev Bektas <bektasmutiev04@mail.ru>
2023-04-05 17:52:22 +0600

    to be continued

---
+++ index.js
@@ -0,0 +1,13 @@
+function MyButton() {
+    return (
+        <button>I am button</button>
+    );
+}
+export default function MyIndex() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
